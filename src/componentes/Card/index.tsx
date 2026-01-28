f41d0f664fadad5e645a8e33af2997118bbea133
Guilherme França <guilfranca27@gmail.com>
2026-01-28 19:04:58 -0300

    init course

---
+++ src/componentes/Card/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+type CardProps = {
+  id: string;
+  imagemUrl: string;
+  titulo: string;
+  resumo: string;
+};
+
+export default function Card({ id, imagemUrl, titulo, resumo }: CardProps) {
+  return (
+    <article className="card" id={id}>
+      <div className="card__imagem">
+        <img src={imagemUrl} alt="imagem do post" />
+      </div>
+      <div className="card__conteudo">
+        <div className="conteudo__texto">
+          <h3>{titulo}</h3>
+          <p>{resumo}</p>
+        </div>
+
+        <div className="conteudo__botoes">
+          <Link to={`/editar-publicacao/${id}`} className="botao__editar">
+            Editar
+          </Link>
+          <button className="botao__deletar">Apagar</button>
+        </div>
+      </div>
+    </article>
+  );
+}
