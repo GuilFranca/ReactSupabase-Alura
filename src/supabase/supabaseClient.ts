bbd83cee3007d67f4b39799c04e97aacc5313b79
Guilherme França <guilfranca27@gmail.com>
2026-01-28 21:52:18 -0300

    Aula 1 - 06 Para saber mais: como usar variáveis de ambiente

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://fnoyyqykixbuafxtprcj.supabase.co";
-const chaveApi = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZub3l5cXlraXhidWFmeHRwcmNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2NDE3NjQsImV4cCI6MjA4NTIxNzc2NH0.UwKPsolFsg4S9tP8AyCuYczs7E2g06BTbL8oYL_2TY8";
+const supabaseUrl = process.env.React_APP_SUPABASE_URL as string;
+const chaveApi = process.env.React_APP_SUPABASE_KEY as string;
 
 // Primeiro URL depois a Chave
 export const supabase = createClient(supabaseUrl, chaveApi); // Esta função vai retornar um objeto com o nosso banco de dados dentro do supabase
