5ab99c6b00701b20dbf811d21be0b76f317042a4
Guilherme França <guilfranca27@gmail.com>
2026-01-29 10:56:02 -0300

    Aula 2 - 03 Capturando erros

--- src/supabase/requisicoes.ts
+++ src/supabase/requisicoes.ts
@@ -5,7 +5,14 @@ export function buscarPostagens() {
     // .select("coluna") Define quais colunas serão retornadas "*" -> todas
     // Desse return será retornado uma promisse como um fetch (assync)
     return supabase
-        .from("postagem")
+        .from("Publicação")
         .select("*")
-        .then(({ data }) => data);
+        .then(({ data, error }) => {
+            if (error) {
+                console.error("Erro ao buscar postagens", error.message);
+                return [];
+            }
+
+            return data;
+        });
 }
--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.React_APP_SUPABASE_URL as string;
-const chaveApi = process.env.React_APP_SUPABASE_KEY as string;
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
+// const chaveApi = process.env.REACT_APP_SUPABASE_KEY as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const chaveApi = import.meta.env.VITE_SUPABASE_KEY as string;
 
 // Primeiro URL depois a Chave
 export const supabase = createClient(supabaseUrl, chaveApi); // Esta função vai retornar um objeto com o nosso banco de dados dentro do supabase
